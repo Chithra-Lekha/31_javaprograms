d9072c67a871282939982e7501cf242924d176e5
Chithra-Lekha <79584685+Chithra-Lekha@users.noreply.github.com>
2021-05-27 10:42:06 +0530

    commit up to program 13

--- First 15 programs./Array.java
+++ First 15 programs./Array.java
@@ -4,7 +4,7 @@ public class Array {
     
    int[] arr = {12, 4, 5, 2};
  
-   System.out.println("Accessing Elements of Array:");
+   System.out.println("Accessing Elements of Array in order:");
    System.out.println("First Element: " + arr[0]);
    System.out.println("Second Element: " + arr[1]);
    System.out.println("Third Element: " + arr[2]);
